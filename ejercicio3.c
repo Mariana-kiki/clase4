494c85c25e094262782cd70f5b6ac97554b778ce
Mariana-kiki <acosmariana9@outlook.com>
2025-11-04 10:57:21 -0600

    Create ejercicio3.c

---
+++ ejercicio3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int void() 
+{
+    int numero, suma = 0, contador = 0;
+
+    while (contador < 10 && suma <= 100) 
+    {
+        printf("Introduce un número: ");
+        scanf("%d", &numero);
+
+        suma += numero;
+        contador++;
+    }
+
+    if (contador == 10) 
+    {
+        printf("Se han introducido 10 números.\n");
+    } 
+    else 
+    {
+        printf("La suma de los números introducidos es mayor que 100.\n");
+    }
+}
