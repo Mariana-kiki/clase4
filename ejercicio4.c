a6ff8b6391da7dedb43d9a4f89f1e70e090f45ae
Mariana-kiki <acosmariana9@outlook.com>
2025-11-04 11:06:52 -0600

    Create ejercicio4.c

---
+++ ejercicio4.c
@@ -0,0 +1,45 @@
+/*
+
+#include <stdio.h>
+
+NUMERO_SECRETO 42
+
+void main() 
+{
+    int intento, contador = 0;
+
+    do 
+    {
+        printf("Adivina el número (entre 1 y 100): ");
+        scanf("%d", &intento);
+        contador++;
+
+        if (intento < NUMERO_SECRETO) 
+        {
+            printf("El número secreto es mayor.\n");
+        } 
+        else 
+          if (intento > NUMERO_SECRETO) 
+          {
+            printf("El número secreto es menor.\n");
+          }
+
+        } 
+     while (intento != NUMERO_SECRETO);
+        {
+            printf("¡Correcto! Has adivinado el número en %d intento(s).\n", contador);
+        }
+    if (contador < 5) 
+    {
+        printf("Enhorabuena!\n");
+    } 
+    else 
+      if (contador <= 10) 
+      {
+        printf("No está mal!\n");
+      } 
+    else 
+    {
+        printf("Debe practicar más.\n");
+    }
+}
