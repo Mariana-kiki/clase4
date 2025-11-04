39055df788b651978cd278dfb88a62223429f2d3
Mariana-kiki <acosmariana9@outlook.com>
2025-11-04 11:10:04 -0600

    Create ejercicio5.c

---
+++ ejercicio5.c
@@ -0,0 +1,44 @@
+
+
+#include <stdio.h>
+
+NUMERO_SECRETO 58  
+
+void main() 
+{
+    int intento, contador = 0;
+    int ha_ganado = 0;
+
+    while (contador < 5) 
+    {
+        printf("Intento %d: Adivina el número (entre 1 y 100): ", contador + 1);
+        scanf("%d", &intento);
+        contador++;
+
+        if (intento == NUMERO_SECRETO) 
+        {
+            ha_ganado = 1;
+            break;
+        } 
+        else 
+          if (intento < NUMERO_SECRETO) 
+          {
+            printf("El número secreto es mayor.\n");
+          } 
+          else 
+          {
+            printf("El número secreto es menor.\n");
+          }
+     }
+
+    if (ha_ganado) 
+    {
+        printf("¡Correcto! Has adivinado el número en %d intento(s).\n", contador);
+        printf("¡Has ganado!\n");
+    } 
+    else 
+    {
+        printf("Lo siento, has agotado los 5 intentos. El número era %d.\n", NUMERO_SECRETO);
+        printf("No has ganado esta vez.\n");
+    }
+}
